9d15693ecb23a1ffbded657759a010213b15f331
elseptiawan <septiawanelshandi@gmail.com>
2023-07-10 20:18:23 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/about', (req, res) => {
+    res.send('About Page');
+});
+
+app.use((req, res) => {
+    res.status(404).send('Not Found');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
